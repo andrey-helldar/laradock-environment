fd9af0dfab17f80839e2b603ad449e8a9542678e
Andrey Helldar <helldar@ai-rus.com>
2021-12-30 13:52:56 +0300

    Removed PHP 7.3 support

--- bin/7.3/only.cmd
+++
@@ -1,7 +0,0 @@
-@echo off
-
-cd ..\..\shell
-
-call variables.cmd
-
-call php-only.cmd "7.3" "vc15"
--- bin/7.3/rebuild.cmd
+++
@@ -1,7 +0,0 @@
-@echo off
-
-cd ..\..\shell
-
-call variables.cmd
-
-call main.cmd "7.3" "vc15" "build"
